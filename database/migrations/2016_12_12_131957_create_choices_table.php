377948ceff9bd998c526bfeee4c21950a89a5f97
mikan835 <mijessi.k@gmail.com>
2016-12-12 14:25:34 +0100

    migrate choices

---
+++ database/migrations/2016_12_12_131957_create_choices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('question_id')->nullable();
+            $table->string('content', 100);
+            $table->enum('status',['yes','no']);
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
