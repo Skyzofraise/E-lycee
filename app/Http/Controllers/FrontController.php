a8ee1041eb48fb7e58b47f45782bdb560193c595
mikan835 <mijessi.k@gmail.com>
2016-12-13 10:04:59 +0100

    ca s'affiche !!!

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -15,57 +15,7 @@ class FrontController extends Controller
 	public function index() 
 	{
 		$posts = Post::all();
-
 		return view('front.home', compact('posts'));
 	}
-
-	// public function userPost($id) {
-	// 	$user = User::find($id);
-	// $name = $user->name;
-	// 	$posts = $user->posts;
-
-	// 	return view('front.user', compact('name', 'posts'));
-	// }
-	// public function PostByTag($id) {
-	// 	$tag = Tag::find($id);
-	// $name = $tag->name;
-	// 	$posts = $tag->posts;
-
-	// 	return view('front.tag', compact('name', 'posts'));
-	// }
-
-	// public function single($id) {
-	// 	$post = Post::find($id);
-	// 	return view('single', ['post'=> $post]);
-	// }
-	// public function student($id) {
-	// 	$student = Student::find($id);
-	// 	return view('student', ['student'=> $student]);
-	// }
-	// public function user($id) {
-	// 	$user = User::find($id);
-	// 	return view('user', ['user'=> $user]);
-	// }
-
-    // public function show($id)
-    // {
-    //     $post = Post::find($id);
-    //     return view('front.show', ['post' => $post]);
-    // }
-    // public function showStudent($id)
-    // {
-    //     $student = Student::find($id);
-    //     return view('front.student.show', compact('student'));
-    // }
-    // public function showPostByCat($id)
-    // {
-    //     $category = Category::find($id);
-    //     $title = $category->title;
-    //     $posts = $category->posts;
-    //     return view('category', [
-    //         'title' => $title,
-    //         'posts' => $posts
-    //     ]);
-    // }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::pattern('id', '[1-9][0-9]*'); // * 0, N
+// Route::pattern('slug', '[a-zA-Z\-]+');   // + 1, N
+
+Route::get('/', 'FrontController@index');
+Route::get('user/{id}', function ($id) {
+    return 'User '.$id;
 });
+
+// Route::get('/post/{id}/{slug?}', 'FrontController@show');
+// Route::get('/category/{id}/{slug?}', 'FrontController@showPostByCat');
+// Route::get('/student/{id}', 'FrontController@showStudent');
+// Route::get('/user/{id}', 'FrontController@showPostByUser');
+// Route::get('/tag/{id}', 'FrontController@showPostByTag');
+
+// Route::any('login','LoginController@login');
+// Route::get('logout', 'LoginController@logout');
+
+// Route::resource('admin/post', 'PostController');
