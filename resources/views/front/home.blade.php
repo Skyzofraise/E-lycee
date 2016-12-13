17b78a889b3a7d8f68e2560dad30c8759c0eea74
mikan835 <mijessi.k@gmail.com>
2016-12-13 15:18:07 +0100

    css articles

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,22 +2,41 @@
 
 @section('content')
     @if(!empty($posts))
-        <div>
+        <section id="articles" class="flex">
             @foreach($posts as $post)
-            <article>
-                @if(!empty($post->url_thumbnail))
-                <img src="{{ url('images/posts', [$post->url_thumbnail]) }}" alt="">
-                @endif
-                <div class="infos">
-                    <h3><a href="{{url('post', [$post->id])}}">{{$post->title}}</a></h3>
-                    <span>{{ $post->date }}</span>
-                    <p>{{ $post->abstract }}</p>
+            <article class="posts">
+                <div class="posts-image">
+                    @if(!empty($post->url_thumbnail))
+                    <img src="{{ url('images/posts', [$post->url_thumbnail]) }}" alt="">
+                    @endif
+                </div>
+                <div class="infos flex-col">
+                    <div class="infos flex">
+                        <div class="posts-date">{{ $post->date }}</div>
+                        <div class="posts-heading flex-col">
+                            <h2 class="posts-title">
+                                <a href="{{url('post', [$post->id])}}">{{$post->title}}</a>
+                            </h2>
+                            <span class="posts-auteur">
+                                <a href="">Quelqu'un</a>
+                            </span>
 
-                    <a href="{{ action('FrontController@actualite', $post->id)}}">Lire la suite</a>
+                            <!-- -->
+                            <!-- AJOUTER AUTEUR/USER -->
+                            <!-- -->
+
+                        </div>
+                    </div>
+                    <div class="posts-resume">
+                        {{ $post->abstract }}
+                    </div>
+                    <div class="posts-readmore">
+                        <a href="{{ action('FrontController@actualite', $post->id)}}">Lire la suite</a>
+                    </div>
                 </div>
             </article>
             @endforeach
-        </div>
+        </section>
     @else
         <p>Il n'y a aucun article pour le moment. Mais cela ne saurait tarder.</p>
     @endif
