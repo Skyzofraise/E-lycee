ee2ad5b12e87bed46fdfb4271d6ceba1ef276e8c
mikan835 <mijessi.k@gmail.com>
2016-12-15 17:15:42 +0100

    derniere modif qui fonctionnne pas

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,34 +8,34 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
-    {
-        if($request->isMethod('post'))
-        {
-            $this->validate($request, [
-                'username' => 'required',
-                'password' => 'required'
-            ]);
+    // public function login(Request $request)
+    // {
+    //     if($request->isMethod('post'))
+    //     {
+    //         $this->validate($request, [
+    //             'username' => 'required',
+    //             'password' => 'required'
+    //         ]);
 
-            // récupère un tableau associatif username password
-            $credentials = $request->only('username', 'password');
+    //         // récupère un tableau associatif username password
+    //         $credentials = $request->only('username', 'password');
 
-            if(Auth::attempt($credentials))
-            {
-                // C'est ok, on a acces au routes protéger
-                return redirect('admin/post')->with(['message'=>'success']);
-            } else {
-                return back()
-                    ->withInput($request->only('username'))
-                    ->with(['message'=> 'C\'est pas bon ça, mon coco !']);
-            }
-        }
-        return view('front.mentions');
-    }
+    //         if(Auth::attempt($credentials))
+    //         {
+    //             // C'est ok, on a acces au routes protéger
+    //             return redirect('admin/post')->with(['message'=>'success']);
+    //         } else {
+    //             return back()
+    //                 ->withInput($request->only('username'))
+    //                 ->with(['message'=> 'C\'est pas bon ça, mon coco !']);
+    //         }
+    //     }
+    //     return view('front.mentions');
+    // }
 
-    public function logout()
-    {
-        Auth::logout();
-        return redirect('/')->with(['message' =>'succes logout']);
-    }
+    // public function logout()
+    // {
+    //     Auth::logout();
+    //     return redirect('/')->with(['message' =>'succes logout']);
+    // }
 }
