fcc72648f2adeb945f6b2e3f89c4b4305a11feb3
LolitaG <lolita@anadore.fr>
2016-12-16 10:23:22 +0100

    image lycée

--- resources/views/front/lycee.blade.php
+++ resources/views/front/lycee.blade.php
@@ -5,19 +5,37 @@
 <h3>Lycée</h3>
 
 <div class="text-full-page">
-	<p>Hac ex causa conlaticia stipe Valerius humatur ille Publicola et subsidiis amicorum mariti inops cum liberis uxor alitur Reguli et dotatur ex aerario filia Scipionis, cum nobilitas florem adultae virginis diuturnum absentia pauperis erubesceret patris.</p>
-
-	<p>Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu et agiliter praeterito Calycadni fluminis ponte, cuius undarum magnitudo murorum adluit turres, in speciem locavere pugnandi. neque tamen exiluit quisquam nec permissus est congredi. formidabatur enim flagrans vesania manus et superior numero et ruitura sine respectu salutis in ferrum.</p>
-
-	<p>Constituendi autem sunt qui sint in amicitia fines et quasi termini diligendi. De quibus tres video sententias ferri, quarum nullam probo, unam, ut eodem modo erga amicum adfecti simus, quo erga nosmet ipsos, alteram, ut nostra in amicos benevolentia illorum erga nos benevolentiae pariter aequaliterque respondeat, tertiam, ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
-
-	<p>Quis enim aut eum diligat quem metuat, aut eum a quo se metui putet? Coluntur tamen simulatione dumtaxat ad tempus. Quod si forte, ut fit plerumque, ceciderunt, tum intellegitur quam fuerint inopes amicorum. Quod Tarquinium dixisse ferunt, tum exsulantem se intellexisse quos fidos amicos habuisset, quos infidos, cum iam neutris gratiam referre posset.</p>
-
-	<p>Orientis vero limes in longum protentus et rectum ab Euphratis fluminis ripis ad usque supercilia porrigitur Nili, laeva Saracenis conterminans gentibus, dextra pelagi fragoribus patens, quam plagam Nicator Seleucus occupatam auxit magnum in modum, cum post Alexandri Macedonis obitum successorio iure teneret regna Persidis, efficaciae inpetrabilis rex, ut indicat cognomentum.</p>
-
-	<p>Hac ex causa conlaticia stipe Valerius humatur ille Publicola et subsidiis amicorum mariti inops cum liberis uxor alitur Reguli et dotatur ex aerario filia Scipionis, cum nobilitas florem adultae virginis diuturnum absentia pauperis erubesceret patris.Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu et agiliter praeterito Calycadni fluminis ponte, cuius undarum magnitudo murorum adluit turres, in speciem locavere pugnandi. neque tamen exiluit quisquam nec permissus est congredi. formidabatur enim flagrans vesania manus et superior numero et ruitura sine respectu salutis in ferrum.</p>
-
-	<p>Quis enim aut eum diligat quem metuat, aut eum a quo se metui putet? Coluntur tamen simulatione dumtaxat ad tempus. Quod si forte, ut fit plerumque, ceciderunt, tum intellegitur quam fuerint inopes amicorum. Quod Tarquinium dixisse ferunt, tum exsulantem se intellexisse quos fidos amicos habuisset, quos infidos, cum iam neutris gratiam referre posset.</p>
+	<h4>Présentation du lycée</h4>
+	<p>
+		Le lycée DIDEROT est installé sur les hauteurs du 19ème arrondissement de PARIS, près du parc des Buttes Chaumont, il fait partie de l'histoire de cet arrondissement depuis 1873. Aujourd'hui situé au 61 rue David d'Angers, son architecture contemporaine « le paquebot » offre un cadre de travail vaste et spacieux pour l'ensemble de la communauté éducative.
+	</p>
+	<p>
+		C'est un établissement de référence pour l'enseignement technologique industriel. Il prépare ses étudiants à sept brevets de techniciens supérieurs industriels dans les domaines de l'informatique de réseau, de la métallurgie, des microtechniques, de la conception de produits industriels, de l'électronique, de l'électrotechnique et des automatismes. Une classe préparatoire aux grandes écoles, dédiée aux techniciens supérieurs, permet aux meilleurs étudiants d'accéder aux métiers d'ingénieurs.
+	</p>
+	<p>
+		<img src="images/school-slice.jpg" alt="">
+	</p>
+	<h4>Nos formations</h4>
+	<p>
+		Deux licences professionnelles construites en partenariat avec des universités constituent une autre voie complémentaire de réussite et de professionnalisation pour nos techniciens supérieurs.
+	</p>
+	<p>
+		Cinq baccalauréats, un scientifique à deux spécialités (sciences de l'ingénieur et sciences et vie de la terre) et quatre STI2D (AC, EE, SIN, ITEC).
+	</p>
+	<p>
+		Par ailleurs depuis la rentrée 2005, le lycée DIDEROT accueille des formations préparant au baccalauréat professionnel dans les domaines de l'étude et de la définition de produits industriels et des microtechniques. Depuis cette même date le lycée forme aux métiers de l'horlogerie et délivre le Brevet des Métiers d'Art ainsi que le CAP et depuis 2015 le Diplôme des Métiers d'Art. Nous proposons ainsi des perspectives de réussite pour les jeunes collégiens dans des secteurs d'activités porteurs.
+		A la rentrée de 2012 le lycée a ouvert une ULIS PRO qui permet à des jeunes en situation de handicap de construire un parcours à la fois scolaire et professionnel.
+	</p>
+	<h4>Le GRETA</h4>
+	<p>
+		Le GRETA Industriel des Technologies Avancées (GPI2D) installé dans nos murs propose des formations en alternance et dans le cadre de la formation continue. Ces formations dans les domaines de la conception de produits, de l'électrotechnique de l'électronique de l'informatique et d'internet prennent appui sur les ressources matérielles et humaines du lycée DIDEROT.
+	</p>
+	<p>
+		Les ressources tant humaines que matérielles dont dispose le lycée DIDEROT permettent d'assurer des opérations de conseil technologique en direction des entreprises.
+	</p>
+	</div>
+		Voué depuis des générations à l'excellence dans le domaine des technologies industrielles, le lycée DIDEROT constitue un outil de promotion et de réussite pour tous les élèves et les étudiants qui lui sont confiés.
+	</p>
 </div>
 
 @endsection
