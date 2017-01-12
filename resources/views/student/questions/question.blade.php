8525190ce3e71cf691680ac171a8e6843eea6627
mikan835 <mijessi.k@gmail.com>
2017-01-12 12:46:54 +0100

    petites suppressions

--- resources/views/student/questions/question.blade.php
+++ resources/views/student/questions/question.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.back')
+@extends('layouts.student-back')
 
 
 @section('content')
@@ -8,12 +8,8 @@
     <h2>{{ $question->content }}</h2>
 
     {!! BootForm::open()->post()->route( 'StudentController@validation', $question )->enctype('multipart/form-data') !!}
-    <!-- {!! BootForm::open()->post()->route( 'QuestionController@ChoiceUpdate', $question ) !!} -->
-    <!-- {!! BootForm::open()->post()->action( route('questions.store') ) !!} -->
 
         {{ csrf_field() }}
-
-        <!-- {!! BootForm::hidden('question_id')->value($question->id) !!} -->
         
         @foreach($choices as $index => $choice)
 
